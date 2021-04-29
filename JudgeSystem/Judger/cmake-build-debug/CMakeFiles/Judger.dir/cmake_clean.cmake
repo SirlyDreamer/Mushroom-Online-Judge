a37eecd32416b5ba9138e20f9108b6e6b37e7db2
SirlyDreamer <huanghongsen@outlookc.com>
2021-04-29 23:54:47 +0800

    Judger Update

---
+++ JudgeSystem/Judger/cmake-build-debug/CMakeFiles/Judger.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Judger.dir/main.cpp.o"
+  "Judger"
+  "Judger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Judger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

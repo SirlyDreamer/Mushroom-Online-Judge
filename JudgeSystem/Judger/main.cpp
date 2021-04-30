7393f7cfa45805cbb31ab8125f5e1203299bfe75
SirlyDreamer <huanghongsen@outlookc.com>
2021-04-30 11:12:28 +0800

    Judger Update

--- JudgeSystem/Judger/main.cpp
+++ JudgeSystem/Judger/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <fstream>
+
 using namespace std;
 
-enum{c = 0, cpp, java, python3, kotlin};
+enum{c = 0, cpp, java, python3, kotlin};//python3 ang kotlin is not supported right now (QwQ)
 
 struct config
 {
@@ -24,7 +26,7 @@ config getConfig()
 	//To test
 	config dev;
 	dev.lang = cpp;
-	dev.source = "./source.cpp";
+	dev.source = "source.cpp";
 	dev.cpuLimit = 2;
 	dev.memLimit = 256;
 	dev.casenum = 1;
@@ -34,11 +36,34 @@ config getConfig()
 
 void initEnv (string source)
 {
-	command[c] = "gcc -g -O2 -std=gnu11 -static " + source + " -lm";
-	command[cpp] = "g++ -g -O2 -std=gnu++17 -static " + source;
-	command[java] = "javac -encoding UTF-8 -sourcepath . -d . " + source;
-	command[python3] = "pypy3 -m py_compile " + source;
-	command[kotlin] = "kotlinc -d . " + source;
+	command[c] = "gcc -g -O2 -std=gnu11 -static " + source + " -lm &> compile.res";
+	command[cpp] = "g++ -g -O2 -std=gnu++17 -static " + source +" &> compile.res";
+	command[java] = "javac -encoding UTF-8 -sourcepath . -d . " + source + " &> compile.res";
+	command[python3] = "pypy3 -m py_compile " + source + " &> compile.res";
+	command[kotlin] = "kotlinc -d . " + source + " &> compile.res";
+}
+
+bool isCE()
+{
+	fstream res;
+	bool ce;
+	string line;
+	res.open("./compile.res",ios::in);
+	if(!res.is_open())
+	{
+		cout << "System Error" << endl;
+		return true;
+	}
+	while(!res.eof())
+	{
+		res >> line;
+		if(line.size() == 0)
+			continue;
+		if(line.find("error")!=line.size())
+			return true;
+	}
+	res.close();
+	return false;
 }
 
 int main ()
@@ -46,5 +71,23 @@ int main ()
 	config src;
 	src = getConfig();
 	initEnv(src.source);
-
+	system(command[src.lang].c_str());
+	if(isCE())
+	{
+		ifstream errorinfo("./compile.res");
+		char buffer[130];
+		if(!errorinfo.is_open())
+		{
+			cout << "System Error" << endl;
+			return 0;
+		}
+		cout << "Compile Error" << endl;
+		while(!errorinfo.eof())
+		{
+			errorinfo.getline(buffer,128);
+			cout << buffer << endl;
+		}
+		return 0;
+	}
+	return 0;
 }
